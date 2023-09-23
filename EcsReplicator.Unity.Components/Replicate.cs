bfb28baf5a555fef28f48c0def682476a46d4719
Peter Bjorklund <544158+piot@users.noreply.github.com>
2023-09-23 09:50:19 +0200

    Initial commit
    chore: initial commit. moved from piot.
    
    chore: update package.json
    
    chore: readme

---
+++ EcsReplicator.Unity.Components/Replicate.cs
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+using Unity.Entities;
+
+namespace EcsReplicator.Unity.Components
+{
+    public struct Replicate : IComponentData
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Ecs Replicator Unity Components
+
+Copyright (c) 2023 Peter Bjorklund. All rights reserved.
