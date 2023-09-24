c17babc3737024f94004eb4e0328747dada4da95
Peter Bjorklund <piot@hotmail.com>
2023-09-24 20:48:16 +0200

    chore: update header

--- EcsReplicator.Unity.Components/Replicate.cs
+++ EcsReplicator.Unity.Components/Replicate.cs
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/ecsreplicator
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
